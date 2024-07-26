780720c410172a023784c0fe1091bd2add5bc832
Jack Anderson <whotfisjax@gmail.com>
2024-07-26 16:18:17 -0400

    v0

---
+++ src/Sparkles.js
@@ -0,0 +1,123 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { cn } from "./lib/utils";
+import { motion, useAnimation } from "framer-motion";
+
+export const SparklesCore = ({
+  id,
+  className,
+  background,
+  minSize,
+  maxSize,
+  speed,
+  particleColor,
+  particleDensity,
+}) => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const controls = useAnimation();
+
+  const particlesLoaded = async (container) => {
+    if (container) {
+      console.log(container);
+      controls.start({
+        opacity: 1,
+        transition: {
+          duration: 1,
+        },
+      });
+    }
+  };
+
+  return (
+    <motion.div animate={controls} className={cn("opacity-0", className)}>
+      {init && (
+        <Particles
+          id={id || "tsparticles"}
+          className={cn("h-full w-full")}
+          particlesLoaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: background || "#0d47a1",
+              },
+            },
+            fullScreen: {
+              enable: false,
+              zIndex: 1,
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: particleColor || "#ffffff",
+              },
+              move: {
+                enable: true,
+                speed: {
+                  min: 0.1,
+                  max: speed || 1,
+                },
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: particleDensity || 120,
+              },
+              opacity: {
+                value: {
+                  min: 0.1,
+                  max: 1,
+                },
+                animation: {
+                  enable: true,
+                  speed: speed || 4,
+                },
+              },
+              size: {
+                value: {
+                  min: minSize || 1,
+                  max: maxSize || 3,
+                },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      )}
+    </motion.div>
+  );
+};
